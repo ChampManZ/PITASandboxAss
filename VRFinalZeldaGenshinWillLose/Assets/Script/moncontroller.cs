ed272aa55661cce999281e74af52b1bf14394706
naphat_c <63011191@kmitl.ac.th>
2021-12-15 04:25:25 +0700

    attacking system

--- VRFinalZeldaGenshinWillLose/Assets/Script/moncontroller.cs
+++ VRFinalZeldaGenshinWillLose/Assets/Script/moncontroller.cs
@@ -4,15 +4,73 @@ using UnityEngine;
 
 public class moncontroller : MonoBehaviour
 {
+    private int mon_hp = 50;
+    private int attacking = 0;
+    [SerializeField] private Animator monAnim;
+    private GameObject myplayer;
+    private float die_timer = 3;
+    private float atk_timer = 2;
+
     // Start is called before the first frame update
     void Start()
     {
+        myplayer = GameObject.Find("Main Camera");
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        float Dist = Vector3.Distance(myplayer.transform.position, transform.position);
+        playercontrol pScript = myplayer.GetComponent<playercontrol>();
+
+        Debug.Log(Dist.ToString());
+        Debug.Log(mon_hp);
+        
+
+        if (Dist <= 2){
+            monAnim.SetInteger("monact", 1);
+            if (attacking == 0){
+                attacking = 1;
+            }else if(attacking == 1 && atk_timer >0){
+                atk_timer -= Time.deltaTime;
+            }else if (attacking == 1 && atk_timer <= 0){
+                attacking = 2;
+            }else if(attacking == 2){
+                atk_timer = 2;
+                attacking = 0;
+                pScript.health -= 1;
+
+            }
+        }else{
+            monAnim.SetInteger("monact", 0);
+            attacking = 0;
+            atk_timer = 2;
+        }
+
+        if(mon_hp <= 0){
+            attacking =0;
+            atk_timer = 2;
+            die_timer -= Time.deltaTime;
+            monAnim.SetInteger("monact", 2);
+        }
+        if (die_timer <= 0){
+            pScript.mon_count += 1;
+            Destroy(gameObject);
+
+        }
         
     }
+
+    private void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.tag == "swordtag"){
+            mon_hp -= 1;
+        }
+        
+        
+    }
+
+
+
 }
--- VRFinalZeldaGenshinWillLose/Assets/Script/playercontrol.cs
+++ VRFinalZeldaGenshinWillLose/Assets/Script/playercontrol.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class playercontrol : MonoBehaviour
 {
+    public int health = 30;
+    public int endphase = 0;
+    public int mon_count =0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,22 @@ public class playercontrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(health);
+        if (health <= 0){
+            Debug.Log("lost");
+        }
+
+        if(mon_count == 10){
+            endphase = 1;
+            mon_count = 0;
+        }
+
+
+
+        if (endphase == 1){
+            health = 30;
+            endphase = 0;
+        }
         
     }
 }
