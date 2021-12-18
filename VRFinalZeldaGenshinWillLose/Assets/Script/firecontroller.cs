7d22955f6d8cf7233a50ac6f6ae8de64d96a5589
naphat_c <63011191@kmitl.ac.th>
2021-12-18 23:50:20 +0700

    firemon

--- VRFinalZeldaGenshinWillLose/Assets/Script/firecontroller.cs
+++ VRFinalZeldaGenshinWillLose/Assets/Script/firecontroller.cs
@@ -12,14 +12,16 @@ public class firecontroller : MonoBehaviour
     private float sy;
     private float sz;
     private GameObject myplayer;
-    private float die_timer = 4;
+    [SerializeField] private float die_timer = 2;
     private int die = 0;
-    [SerializeField] private float speed = 7;
+    [SerializeField] private float speed = 20;
+    [SerializeField] private Animator fireAnim;
 
 
     void Start()
     {
         myplayer = GameObject.Find("Main Camera");
+        //fireAnim.SetInteger("fireact", 0);
         
     }
 
@@ -32,10 +34,19 @@ public class firecontroller : MonoBehaviour
         if(Dist < 1 && die == 0){
             pScript.health -= 8;
         }
+        if(die == 1 && die_timer > 0 ){
+            die_timer -= Time.deltaTime;
+
+        }else if (die == 1 && die_timer <= 0){
+            die_timer = 0;
+            Destroy(gameObject);
+        }
 
 
         if(dashing == 0){
+            fireAnim.SetInteger("fireact", 0);
         if (dash < 5){
+            
 
             if(cooldown > 0){
                 cooldown -= Time.deltaTime;
@@ -57,6 +68,7 @@ public class firecontroller : MonoBehaviour
             //die* and set act
             die = 1;
             dashing = 2;
+            fireAnim.SetInteger("fireact", 3);
         }
         
     }else{
@@ -64,9 +76,12 @@ public class firecontroller : MonoBehaviour
         if(transform.position.x  != sx && transform.position.y != sy && transform.position.z != sz){
             dashing = 1;
             transform.position = Vector3.MoveTowards(transform.position, new Vector3(sx,sy,sz), Time.deltaTime * speed);
+            fireAnim.SetInteger("fireact", 1);
 
         }else{
+            dash += 1;
             dashing = 0;
+            fireAnim.SetInteger("fireact", 2);
             cooldown = 5;
         }
         
